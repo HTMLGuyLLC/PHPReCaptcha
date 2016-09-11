127ee54fa6503e2d779165dcd0f75404c79f9afd
Shane Stebner <shane@verswerks.com>
2016-09-11 13:02:56 -0400

    Update Sample.php

--- Sample.php
+++ Sample.php
@@ -1,8 +1,11 @@
 <?php
+//include the class
 include('ReCaptcha.php');
 
+//if the form was posted
 if( !empty($_POST) )
 {
+  //verify the captcha
   if( !ReCaptcha::verify() )
   {
     echo 'Failed!<br>';
@@ -15,10 +18,12 @@ if( !empty($_POST) )
 ?>
 <html>
   <head>
+  <!-- Include Google's JS -->
   <script src='https://www.google.com/recaptcha/api.js'></script>
   </head>
   <body>
     <form action='Sample.php' method='post'>
+      <!-- Display the captcha here -->
       <?php
       echo ReCaptcha::display();
       ?>
